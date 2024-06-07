9fbd58bdff35929b7141ae3380f61d1d44986b93
denverlee00 <167231684+denverlee00@users.noreply.github.com>
2024-06-07 20:42:22 +0800

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyCtb_6vhe2g1Fg98jLiJ6ZXJPGr3SG95nQ",
+  authDomain: "crud-f9bb3.firebaseapp.com",
+  projectId: "crud-f9bb3",
+  storageBucket: "crud-f9bb3.appspot.com",
+  messagingSenderId: "861489858922",
+  appId: "1:861489858922:web:d3ef8b72c0f299b0e47d13"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
